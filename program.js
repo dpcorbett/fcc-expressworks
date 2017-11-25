fa02b8d95a48ca107b6d589026e6df5361a95e57
Dave Corbett <davidpcorbett@gmail.com>
2017-11-25 10:05:35 +0000

    Good Old Form

--- program.js
+++ program.js
@@ -1,9 +1,8 @@
-var path = require('path')
 var express = require('express')
+var bodyparser = require('body-parser')
 var app = express()
-app.set('views', (process.argv[3]||path.join(__dirname, 'templates')))
-app.set('view engine', 'jade')
-app.get('/home', function(req, res) {
-    res.render('index', {date: new Date().toDateString()})
+app.use(bodyparser.urlencoded({extended: false}))
+app.post('/form', function(req, res) {
+    res.send(req.body.str.split('').reverse().join(''))
 })
 app.listen(process.argv[2])
