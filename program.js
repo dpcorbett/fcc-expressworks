76e00c0735d8bbcb95a05a1f26cf7008624ee103
Dave Corbett <davidpcorbett@gmail.com>
2017-11-25 09:45:15 +0000

    Jade

--- program.js
+++ program.js
@@ -1,5 +1,9 @@
-var express = require('express')
 var path = require('path')
+var express = require('express')
 var app = express()
-app.use(express.static(process.argv[3]||path.join(__dirname, 'public')));
+app.set('views', (process.argv[3]||path.join(__dirname, 'templates')))
+app.set('view engine', 'jade')
+app.get('/home', function(req, res) {
+    res.render('index', {date: new Date().toDateString()})
+})
 app.listen(process.argv[2])
